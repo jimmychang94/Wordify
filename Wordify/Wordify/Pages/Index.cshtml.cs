ef4706b8a92a559e5359e8a511fb2c20dfe6707c
NateA275 <nateaurich@gmail.com>
2018-08-06 11:25:46 -0700

    Beginning CogServ setup

--- Wordify/Wordify/Pages/Index.cshtml.cs
+++ Wordify/Wordify/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,18 @@ namespace Wordify.Pages
 {
     public class IndexModel : PageModel
     {
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+
         public void OnGet()
         {
+
+        }
+
+        public Task<IActionResult> OnPost()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
 
         }
     }
