251cf8c3f8e074fd267a920736b30155ed0d6a80
Jimmy Chang <jimmyfchang94@gmail.com>
2018-08-08 18:17:39 -0700

    notes now show up, currently set up to show on the profile page

--- Wordify/Wordify/Pages/Profile.cshtml.cs
+++ Wordify/Wordify/Pages/Profile.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Wordify.Data;
+using Wordify.Extensions;
 using Wordify.Models;
 using Wordify.Models.Interfaces;
 
@@ -23,6 +24,7 @@ namespace Wordify.Pages
         private UserManager<ApplicationUser> _userManager;
         private SignInManager<ApplicationUser> _signInManager;
         private INote _notes;
+        private IBlob _blob;
 
         [BindProperty]
         public string FirstName { get; set; }
@@ -42,12 +44,18 @@ namespace Wordify.Pages
         [BindProperty]
         public List<Note> Notes { get; set; }
 
+        [BindProperty]
+        public Note Note { get; set; }
+
+        public string Text { get; set; }
+
         public ProfileModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,
-            INote notes)
+            INote notes, IBlob blob)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _notes = notes;
+            _blob = blob;
         }
 
         public async Task<IActionResult> OnGet()
@@ -63,7 +71,7 @@ namespace Wordify.Pages
             return Page();
         }
 
-        public async Task<IActionResult> OnPost()
+        public async Task<IActionResult> OnPostUser()
         {
             var user = await _userManager.GetUserAsync(User);
 
@@ -86,5 +94,29 @@ namespace Wordify.Pages
             return RedirectToPage("/Index");
         }
 
+        public void OnPostNote(int id)
+        {
+            var user = _userManager.GetUserAsync(User).Result;
+
+            UserName = user.UserName;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            Bio = user.Bio;
+            Email = user.Email;
+            Notes = _notes.GetNotesByUserID(user.Id).Result;
+            try
+            {
+                Note note = _notes.GetNoteByID(id).Result;
+                string blobText = _blob.GetText(note).Result;
+                Note = note;
+                Text = blobText;
+                byte[] blobImage = _blob.GetImage(note).Result;
+                ImageDisplayExtensions.DisplayImage(blobImage);
+            }
+            catch (Exception)
+            {
+                TempData["Error"] = "Something went wrong with the Post Note";
+            }
+        }
     }
 }
